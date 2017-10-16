b5d63680c9d3e3617b6bfe888cf583b3c60628b2
georgyorgy1 <georgyorgy1@users.noreply.github.com>
2017-10-16 14:59:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,123 +1,115 @@
-#Main program
-#Released under the BSD-new (3 clause) license. See COPYING for more information
 import discord
 import asyncio
-import padshitpostlib
-import padmathlib
-import padphysicslib
-import padloglib
-import padgamelib
+import datetime
+import random
+import time
 
-class Main:
-    bot = discord.Client()
-    shitpost = padshitpostlib.Shitpost
-    math = padmathlib.Mathematics
-    physics = padphysicslib.Physics
-    game = padgamelib.Game
-    logger = padloglib.Printer
+bot = discord.Client()
+start_time = time.time()
 
-    def log_string(command, author, author_id, channel, server_name):
-        return command + ' command engaged by ' + str(author) + ' (ID: ' + str(author_id) + ') in #' + str(channel) + ' @ ' + str(server_name)
+@bot.event
+async def about(message):
+    string = "```" + "\n" + "PadmeBot v2.0 Beta. Copyright Darth Squidward" + "\n" + "See m!commands for commands" + "\n" + "```"
+    await bot.send_message(message.channel, string)
+    print(str(datetime.datetime.now()) + ": " + "m!about triggered")
 
-    @bot.event
-    async def on_message(message):
-        try:
-            if message.content.startswith('$checkem'):
-                string = Main.shitpost.check_em()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$checkem', message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def uptime(message):
+    string = "```" + "\n" + "Uptime (in seconds) is: " + str(int(time.time() - start_time)) + "\n" + "```"
+    await bot.send_message(message.channel, string)
+    print(str(datetime.datetime.now()) + ": " + "m!uptime triggered")
+    
+@bot.event
+async def random_number(message):
+    num = random.randint(0, 2147483647)
+    string = "```" + "\n" + "Here's your number: " + str(num) + "\n" + "```"
+    await bot.send_message(message.channel, string)
+    print(str(datetime.datetime.now()) + ": " + "m!random triggered")
 
-            if message.content.startswith('$daisy'):
-                string = Main.shitpost.daisy()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$daisy', message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def check_datetime(message):
+    current_datetime = time.strftime("%A, %B %d, %Y %I:%M:%S %p")
+    string = "```" + "\n" + "Current date and time (GMT 8:00+): " + str(current_datetime) + "\n" + "```"
+    await bot.send_message(message.channel, string)
+    print(str(datetime.datetime.now()) + ": " + "m!dt/m!time triggered")
 
-            if 'big guy' in message.content:
-                string = Main.shitpost.big_guy()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('big guy', message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def aesthetics(message):
+    aesthetic = open('aesthetics.txt')
+    await bot.send_message(message.channel, random.choice(aesthetic.readlines()))
+    aesthetic.close()
+    print(str(datetime.datetime.now()) + ": " + "m!aesthetics triggered")
 
-            if message.content.startswith('garlic'):
-                string = Main.shitpost.garlic()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('garlic', message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def test_function(message):
+    await bot.send_message(message.channel, "returning message")
+    print(str(datetime.datetime.now()) + ": " + "Test function triggered")
 
-            if message.content.startswith('anakin'):
-                string = Main.shitpost.anakin()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('anakin', message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def print_commands(message):
+    string = open('commands.txt')
+    await bot.send_message(message.channel, string.read())
+    string.close()
+    print(str(datetime.datetime.now()) + ": " + "m!commands triggered")
 
-            if message.content.startswith("who's your daddy?"):
-                string = Main.shitpost.daddy()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string("who's your daddy?", message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def send_nudes(message):
+    await bot.send_message(message.channel, "Feature coming soon")
+    print(str(datetime.datetime.now()) + ": " + "m!nudes triggered")
 
-            if message.content.startswith('weed'):
-                string = Main.shitpost.weed()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('weed', message.author, message.author.id, message.channel, message.server))
+@bot.event
+async def on_message(message):
+    if message.content.startswith('m!loltest') and str(message.author.id) == str(bot.user.id):
+        await test_function(message) #test function
+        
+    if message.content.startswith('m!aesthetics'):
+        await aesthetics(message) #prints aestheics. From AestheticsBot and PadmeBot
 
-            if message.content.startswith('lol'):
-                string = Main.shitpost.lol()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('lol', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!dt'):
+        await check_datetime(message) #checks datetime lol
 
-            if message.content.startswith('$rand32') or '$random' in message.content:
-                string = Main.math.generate_32bit()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$rand32/$random', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!time'):
+        await check_datetime(message) #checks datetime also lol
 
-            if message.content.startswith('$rand64'):
-                string = Main.math.generate_64bit()
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$rand64', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!random'):
+        await random_number(message) #generates a random 32-bit number (from 1 to the 32-bit limit)
 
-            if message.content.startswith('$sin'):
-                number = message.content[len('$sin'):].strip()
-                string = Main.math.get_sin(number)
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$sin', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!uptime'):
+        await uptime(message) #check uptime
 
-            if message.content.startswith('$cos'):
-                number = message.content[len('$cos'):].strip()
-                string = Main.math.get_cos(number)
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$cos', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!about'):
+        await about(message) #about selfbot
 
-            if message.content.startswith('$tan'):
-                number = message.content[len('$tan'):].strip()
-                string = Main.math.get_tan(number)
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$tan', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!commands'):
+        await print_commands(message) #print commands
 
-            if message.content.startswith('$weight'):
-                number = message.content[len('$weight'):].strip()
-                string = str(Main.physics.get_weight(number))
-                await Main.bot.send_message(message.channel, string)
-                Main.logger.say(Main.log_string('$weight', message.author, message.author.id, message.channel, message.server))
+    if message.content.startswith('m!nudes'):
+        await send_nudes(message) #sends nudes
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(game=discord.Game(name='m!commands for commands'))
+    print(str(datetime.datetime.now()) + ": " + "Welcome " + str(bot.user))
+    print(str(datetime.datetime.now()) + ": " + "Current date and time: " + str(datetime.datetime.now()))
+        
+def main():
+    credentials = open('creds.txt')
+    auth = []
+    
+    with credentials as f:
+        auth = f.read().splitlines()
+    
+    credentials.close()
+    
+    try:
+        print(str(datetime.datetime.now()) + ": " + "Starting PadmeBot v2.0. Please wait...")
+        bot.run(auth[0]) #token 
+        auth = ""
+            
+    except discord.errors.LoginFailure:
+        error_string = "Your token is either invalid or empty. Please replace the token in creds.txt with a valid one. Quitting..."
+        print(str(datetime.datetime.now()) + ": " + error_string)
+        quit()
 
-        except Exception as e:
-            Main.logger.say(str(e))
-            await Main.bot.send_message(message.channel, 'Whoops, something went wrong. Tell the bot owner to check the logs for more information!')
-
-    @bot.event
-    async def on_ready():
-        await Main.bot.change_presence(game=discord.Game(name='PadmeBot v2.1 alpha test'))
-        Main.logger.say('PadmeBot is ready!')
-
-    def main():
-        Main.logger.say('Starting PadmeBot.....')
-        token_file = open('cred.txt', 'r')
-        cred = token_file.read()
-        token_file.close()
-
-        try:
-            Main.bot.run(str(cred))
-            cred = ''
-
-        except discord.errors.LoginFailure:
-            error_string = 'Login failed. Your token is either invalid or empty. Please check cred.txt if your token is valid or not empty'
-            Main.logger.say(error_string)
-
-Main.main()
+#go to main()
+main()
