c1fa9ad4506c68350770dbdd58f0a81305b49eb8
georgyorgy1 <georgyorgy1@users.noreply.github.com>
2017-12-21 17:39:29 +0800

    Update padgenlib.py

--- padgenlib.py
+++ padgenlib.py
@@ -2,7 +2,7 @@ import discord
 
 class General:
     def about():
-        return "PadmeBot v2.11 (Build 20171221-1736_stock) \nCopyright Darth Squidward, licensed under the new BSD (3-clause) license. See COPYING for more info. \nSource Code (original): https://github.com/georgyorgy1/padmebot_v2"
+        return "PadmeBot v2.11 (Build 20171221-1739) \nCopyright Darth Squidward, licensed under the new BSD (3-clause) license. See COPYING for more info. \nSource Code (original): https://github.com/georgyorgy1/padmebot_v2"
 
     def commands():
         return "Coming soon!"
